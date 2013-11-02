7dbda00de3a04ffa25e963ffa5512ebccda50c50
Adam Culp <adamculp@uws.net>
2013-11-02 11:36:56 -0400

    Added navigation array item

--- config/module.config.php
+++ config/module.config.php
@@ -32,6 +32,18 @@ return array(
             ),
         ),
     ),
+    'navigation' => array(
+        'default' => array(
+            array(
+                'label' => 'Talks',
+                'route' => 'talks-index',
+                'lastmod' => '2013-09-08',
+                'changefreq' => 'monthly',
+                'priority' => '1.0',
+                'order' => '25',
+            ),
+        ),
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             'SspTalks' => __DIR__ . '/../view',
